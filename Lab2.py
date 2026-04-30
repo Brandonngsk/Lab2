c556ae70a5937f99159cae3267c8e30097034f66
Brandon Ng <ngskbrandon08.25@ichat.sp.edu.sg>
2026-04-30 15:55:50 +0800

    I hate this

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,27 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu()")
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+    
+def get_user_input():
+   x = input()
+   x = x.split(",")
+   float_list = []
+   for num_str in x:
+       float_list.append (float(num_str))
+   return float_list
+
+def calc_average_temperature(list_temp):
+    avg = sum(list_temp) / len(list_temp)
+    return avg
+
+def calc_min_max_temperature(list_max_temp):
+    maxxy = max(list_max_temp)
+    minny = min(list_max_temp)
+    print("maximum = ", maxxy , ",minimum= ", minny)
+    
+float_list = get_user_input()
+
+average = calc_average_temperature(float_list)
+print (average)
+
+calc_min_max_temperature(float_list)

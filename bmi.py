5bd60a24b4d9a7d213e70a1ab99ba75e499fae16
Brandon Ng <ngskbrandon08.25@ichat.sp.edu.sg>
2026-05-07 13:58:02 +0800

    oi boi

--- bmi.py
+++ bmi.py
@@ -4,12 +4,14 @@ def calculate_bmi(weight,height):
     
     bmi= weight/(height**2)
     print("BMI= "+str(bmi))
-    return bmi
 
-bmi = calculate_bmi(weight=57, height=1.73)   
-if bmi < 18.5:
-    print("under weight")
-elif bmi <= 25.0 or 18.5 <= bmi:
-    print("normal weight")
-else:
-    print("overweight")
+    if bmi < 18.5:
+
+        print("under weight")
+        return -1
+    elif 18.5 <= bmi <= 25.0:
+        print("normal weight")
+        return 0
+    else:
+        print("overweight")
+        return 1
